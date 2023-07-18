534b07f9f42624063d46f0f59c8a7cbf1e89330f
ChanoMV <117382340+ChanoMV@users.noreply.github.com>
2023-07-18 16:38:13 -0300

    FB.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,7 @@
 
 
 export const environment={
-  firebase: {
-    projectId: 'proyectoargprogr',
-    appId: '1:94509087537:web:ba20a2ca4669912bbc8e10',
-    storageBucket: 'proyectoargprogr.appspot.com',
-    locationId: 'southamerica-east1',
-    apiKey: 'AIzaSyDiMm4l7b4vgD_FxAVvlLVjNUxDWlTxhDg',
-    authDomain: 'proyectoargprogr.firebaseapp.com',
-    messagingSenderId: '94509087537',
-    measurementId: 'G-HW967KSJ1D',
-  },
+  
     production: false,
 
    //URL: 'http://localhost:8080/', 
