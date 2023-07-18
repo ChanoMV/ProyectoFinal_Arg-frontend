50aefdfa3d17d98b439ac17b57378d7a024bb2f8
ChanoMV <117382340+ChanoMV@users.noreply.github.com>
2023-07-18 12:54:49 -0300

    enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,6 +14,6 @@ export const environment={
     production: false,
 
    //URL: 'http://localhost:8080/', 
-    URL: 'https://backendmr54289.onrender.com/'
+     URL: 'https://backendmr54289.onrender.com/'
     
 }
